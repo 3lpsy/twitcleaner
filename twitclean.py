74825ecad0e0715e7daa3d55d7cdeade0c9c7b97
3lpsy <8757851+3lpsy@users.noreply.github.com>
2021-09-15 15:54:16 -0700

    formatting, hopefully not broke

--- twitclean.py
+++ twitclean.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import Iterator, List, Tuple, Union
 from os import environ
+
 import sys
 from signal import signal, SIGINT
 from pathlib import Path
@@ -9,24 +10,31 @@ from tweepy import OAuthHandler, API, User, RateLimitError, Cursor, Status
 import argparse
 import time
 
+
 def eprint(*args, **kwargs):
-    print(*args,file=sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
+
 
 def stdout(*args, **kwargs):
     print(*args, **kwargs)
     if not sys.stdout.isatty():
         sys.stdout.flush()
 
+
 def sigint(received, frame):
     eprint("SIGINT or CTRL-C received. Giving up.")
     sys.exit(0)
 
+
 def getenv(key):
     val = environ.get(key, None)
     if val is None or not val:
-        eprint(f"[!] Please set environment variable in dotenv file or environment: {key}")
+        eprint(
+            f"[!] Please set environment variable in dotenv file or environment: {key}"
+        )
         sys.exit(1)
     return val
+
 
 def loadenv(env_file):
     env_file = env_file or ".env"
@@ -40,9 +48,10 @@ def loadenv(env_file):
                 pair = line.split("=", 1)
                 if len(pair) == 2:
                     k = pair[0]
-                    val = pair[1].strip("\"")
+                    val = pair[1].strip('"')
                     eprint(f"[*] Loading into environment: {k}")
                     environ[k] = val
+
 
 def make_api():
     CONSUMER_KEY = getenv("TWITTER_CONSUMER_KEY")
@@ -53,40 +62,48 @@ def make_api():
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     return API(auth)
 
-def user_limit(cursor)-> Iterator[List[User]]:
+
+def user_limit(cursor) -> Iterator[List[User]]:
     while True:
         try:
             yield cursor.next()
         except RateLimitError:
             eprint("[!] Rate limit hit. Sleeping for a bit")
-            time.sleep(15 * 60) 
+            time.sleep(15 * 60)
         except StopIteration:
-            eprint("[*] Iteration halted for user listing. Most likely the page limit has been hit")
+            eprint(
+                "[*] Iteration halted for user listing. Most likely the page limit has been hit"
+            )
             break
 
-def status_limit(cursor, username)-> Iterator[Status]:
+
+def status_limit(cursor, username) -> Iterator[Status]:
     while True:
         try:
             yield cursor.next()
         except RateLimitError:
             eprint("[!] Rate limit hit. Sleeping for a bit")
-            time.sleep(15 * 60) 
+            time.sleep(15 * 60)
         except StopIteration:
-            eprint(f"[*] Iteration halted on status for {username}. Most likely the user is inactive.")
+            eprint(
+                f"[*] Iteration halted on status for {username}. Most likely the user is inactive."
+            )
             break
+
 
 Cohort = List[Tuple[str, str, Union[datetime, None]]]
 
-def scan_cohort(api:API, page, date_cutoff: datetime) -> Cohort:
+
+def scan_cohort(api: API, page, date_cutoff: datetime) -> Cohort:
     inactive_cohort: Cohort = []
     for user in page:
         is_active_user = False
         # typing messed up
         udata = user._json
-        user_id: str =  udata["id"]
+        user_id: str = udata["id"]
         user_username: str = udata["screen_name"]
         newest_created_at = None
-        #print(f"[*] Reviewing user {user_username} - {user_id}")
+        # print(f"[*] Reviewing user {user_username} - {user_id}")
         # alternatively just use the model
         scursor = Cursor(api.user_timeline, user_id=user_id).items(3)
         for tweet in status_limit(scursor, user_username):
@@ -95,15 +112,18 @@ def scan_cohort(api:API, page, date_cutoff: datetime) -> Cohort:
             if not newest_created_at:
                 newest_created_at = created_at
             if created_at >= date_cutoff:
-                #print(f"[**] Passes test: {str(created_at)}")
+                # print(f"[**] Passes test: {str(created_at)}")
                 is_active_user = True
                 break
         if not is_active_user:
-            eprint(f"[**] Failed test {user_username}: {str(newest_created_at)} < {date_cutoff}")
+            eprint(
+                f"[**] Failed test {user_username}: {str(newest_created_at)} < {date_cutoff}"
+            )
             u = (user_username, user_id, newest_created_at)
             inactive_cohort.append(u)
     return inactive_cohort
-    
+
+
 def run(username, days_past, env_file=None, max_pages=None):
     if env_file:
         loadenv(env_file)
@@ -124,14 +144,28 @@ def run(username, days_past, env_file=None, max_pages=None):
         time.sleep(5)
     eprint("[*] Listing complete")
 
+
 if __name__ == "__main__":
     signal(SIGINT, sigint)
     parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--env-file", type=str, help="A .env file containing API auth values")
-    parser.add_argument("-d", "--days-past", type=int, default=30, help="List users who have not tweeted in past number of days. (Default: 30)")
-    parser.add_argument("-P", "--max-pages", type=int, help="Maximum number of pages to search. Mostly for testing")
-    parser.add_argument("-u", "--username", type=str, required=True, help="Your username")
+    parser.add_argument(
+        "-e", "--env-file", type=str, help="A .env file containing API auth values"
+    )
+    parser.add_argument(
+        "-d",
+        "--days-past",
+        type=int,
+        default=30,
+        help="List users who have not tweeted in past number of days. (Default: 30)",
+    )
+    parser.add_argument(
+        "-P",
+        "--max-pages",
+        type=int,
+        help="Maximum number of pages to search. Mostly for testing",
+    )
+    parser.add_argument(
+        "-u", "--username", type=str, required=True, help="Your username"
+    )
     args = parser.parse_args()
     run(args.username, args.days_past, args.env_file, args.max_pages)
-
-
